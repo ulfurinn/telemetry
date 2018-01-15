8545f1bdb370b0a5439977d355d927d2427d32ad
Valeri Sokolov <ulfurinn@ulfurinn.net>
2018-01-15 16:34:27 +0100

    fully qualified project name and version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject telemetry "0.1.0-SNAPSHOT"
+(defproject net.ulfurinn/telemetry "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
