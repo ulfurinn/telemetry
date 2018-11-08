ba4157c848e51f9441c99eb5ff40445d2bd8647e
Valeri Sokolov <valeri.sokolov@derivco.se>
2018-11-08 13:34:42 +0100

    skipping signed tags

--- project.clj
+++ project.clj
@@ -3,11 +3,17 @@
   :url "https://github.com/ulfurinn/telemetry"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-  	[org.clojure/clojure "1.8.0"]
-  	[alaisi/postgres.async "0.8.0"]
-  	[org.postgresql/postgresql "42.1.4"]
-  	[migratus "1.0.1"]
-  	[cheshire "5.5.0"]
-  ]
-  :plugins [[migratus-lein "0.5.3"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [alaisi/postgres.async "0.8.0"]
+                 [org.postgresql/postgresql "42.1.4"]
+                 [migratus "1.0.1"]
+                 [cheshire "5.5.0"]]
+  :plugins [[migratus-lein "0.5.3"]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
