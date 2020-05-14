a7bdba2d6c87b46f8f5d5226c603cc522fddc7f0
Valeri Sokolov <ulfurinn@ulfurinn.net>
2020-05-14 08:49:54 +0200

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.ulfurinn/telemetry "0.1.4"
+(defproject net.ulfurinn/telemetry "0.1.5-SNAPSHOT"
   :description "Some helpful Riemann streams."
   :url "https://github.com/ulfurinn/telemetry"
   :license {:name "Eclipse Public License"
