79570aae63d02f97fb6198812ea30d02b799a389
Valeri Sokolov <ulfurinn@ulfurinn.net>
2020-05-14 08:57:43 +0200

    updated db dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [alaisi/postgres.async "0.8.0"]
-                 [org.postgresql/postgresql "42.1.4"]
-                 [migratus "1.0.1"]
+                 [org.postgresql/postgresql "42.2.12"]
+                 [migratus "1.2.8"]
                  [cheshire "5.5.0"]]
   :plugins [[migratus-lein "0.5.3"]
             [lein-pprint "1.2.0"]])
